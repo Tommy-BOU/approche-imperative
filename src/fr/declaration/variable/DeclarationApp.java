d23e51a46caebcc50f1920ecf1bd9f7b5bf6e803
Tommy-BOU <tommy.boubaker@free.fr>
2025-07-07 11:37:42 +0200

    First commit

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,21 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte a = 10;
+        short b = 100;
+        int c = 2000;
+        long d = 50000;
+        float e = 10.20f;
+        double f = 456.58d;
+        char g = 'h';
+        boolean h = true;
+        String test = "test";
+
+        System.out.println(test);
+
+        String randomString = "voici le résultat d'un calcul : \n 1+5=6";
+
+        System.out.println(randomString);
+    }
+}
