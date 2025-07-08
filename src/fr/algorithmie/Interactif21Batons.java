1ad9280b2b034ff80c35aace35d442a2491fca59
Tommy-BOU <tommy.boubaker@free.fr>
2025-07-08 14:51:35 +0200

    corrige typo + add losing condition

--- src/fr/algorithmie/Interactif21Batons.java
+++ src/fr/algorithmie/Interactif21Batons.java
@@ -8,13 +8,12 @@ public class Interactif21Batons {
         int nbBatons = 21;
         Random random = new Random();
 
-        int firstPlayer = random.nextInt(0,2);
+        int firstPlayer = random.nextInt(0, 2);
 
-        if (firstPlayer == 0){
+        if (firstPlayer == 0) {
             System.out.println("Vous commencez");
             PlayerPlay(nbBatons);
-        }
-        else {
+        } else {
             System.out.println("L'ordinateur commence");
             ComputerPlay(nbBatons);
         }
@@ -22,24 +21,22 @@ public class Interactif21Batons {
 
     }
 
-    public static void PlayerPlay(int nbBatons){
+    public static void PlayerPlay(int nbBatons) {
         Scanner scanner = new Scanner(System.in);
         int pickedNumber = 0;
         System.out.println("Choisissez un nombre entre 1 et 3");
-        while (pickedNumber < 1 || pickedNumber > 3){
+        while (pickedNumber < 1 || pickedNumber > 3) {
             pickedNumber = scanner.nextInt();
         }
         nbBatons -= pickedNumber;
 
-        if (nbBatons < 1){
+        if (nbBatons < 1) {
             System.out.println("Batons restant : " + nbBatons);
             System.out.println("Vous avez perdu.");
-        }
-        else if (nbBatons == 1){
+        } else if (nbBatons == 1) {
             System.out.println("Batons restant : " + nbBatons);
-            System.out.println("Vous avez gagner !");
-        }
-        else {
+            System.out.println("Vous avez gagné !");
+        } else {
             System.out.println("Batons restant : " + nbBatons);
             System.out.println("Au tour de l'ordinateur");
             ComputerPlay(nbBatons);
@@ -47,23 +44,26 @@ public class Interactif21Batons {
 
     }
 
-    public static void ComputerPlay(int nbBatons){
+    public static void ComputerPlay(int nbBatons) {
         int pickedNumber = PickRandomNumber(nbBatons);
         System.out.println("L'ordinateur prend " + pickedNumber + " batons.");
         nbBatons -= pickedNumber;
         System.out.println("Batons restant : " + nbBatons);
-        PlayerPlay(nbBatons);
+        if (nbBatons == 1) {
+            System.out.println("Vous avez perdu.");
+        } else {
+            PlayerPlay(nbBatons);
+        }
     }
 
-    public static int PickRandomNumber(int nbBatons){
+    public static int PickRandomNumber(int nbBatons) {
         Random random = new Random();
 
-        int pickedNum = random.nextInt(1,4);
+        int pickedNum = random.nextInt(1, 4);
 
-        if (nbBatons - pickedNum < 1){
+        if (nbBatons - pickedNum < 1) {
             return PickRandomNumber(nbBatons);
-        }
-        else {
+        } else {
             return pickedNum;
         }
     }
